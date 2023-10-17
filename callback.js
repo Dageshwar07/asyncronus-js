0a873a35497f6dfc68fbd734c8961883baa4e7a3
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-10-17 12:43:52 +0530

    promise chaining

--- callback.js
+++ callback.js
@@ -16,59 +16,69 @@
 
 // waitForThreeSecond()
 
-function register(cb) {
-
-    setTimeout(() => {
-
-        console.log("user register")
-        cb()
-    }, 5000)
-
+function register() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("user register")
+            res()
+        }, 5000)
+    })
 }
-function sendemail(cb) {
-    setTimeout(() => {
-        console.log("email send")
-        cb()
-
-    }, 3000);
-
-
-}
-function login(cb) {
-    setTimeout(() => {
-        console.log("user can login")
-        cb()
-
-    }, 2000);
-
-}
-function getUserData(cb) {
-    setTimeout(() => {
-        console.log("get user data")
-        cb()
-
-    }, 1000);
-
-}
-function displayUserData() {
-    setTimeout(() => {
-        console.log("display user data")
-    }, 1000);
-}
-register(function () {
-    sendemail(function () {
-        login(function () {
-            getUserData(function () {
-                displayUserData(function () {
-
-                })
-
-            })
-        })
-
+function sendemail() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("email send")
+            res()
+        }, 3000);
     })
 
-})
+}
+function login() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("user can login")
+            res()
+        }, 2000);
+    })
+
+}
+function getUserData() {
+    return new Promise((res, rej) => {
+
+        setTimeout(() => {
+            console.log("get user data")
+            res()
+
+        }, 1000);
+    })
+}
+function displayUserData() {
+
+    return new Promise((res, rej) => {
+
+        setTimeout(() => {
+            console.log("display user data")
+            res()
+
+        }, 1000);
+    })
+
+}
+
+// register(function () {
+//     sendemail(function () {
+//         login(function () {
+//             getUserData(function () {
+//                 displayUserData()
+//             })
+//         })
+//     })
+// })
+register()
+    .then(sendemail)
+    .then(login)
+    .then(getUserData)
+    .then(displayUserData)
 
 
 console.log("other application work")
