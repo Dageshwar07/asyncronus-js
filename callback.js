ddd6c00382c72c4d8e6d84a7fa1abd986d200125
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-10-17 11:20:20 +0530

    callback-hell

--- callback.js
+++ callback.js
@@ -6,46 +6,69 @@
  * display user data
 */
 
-function waitForThreeSecond() {
-    let wait = 3000 + new Date().getTime()
-    while (new Date() < wait) {
+// function waitForThreeSecond() {
+//     let wait = 3000 + new Date().getTime()
+//     while (new Date() < wait) {
 
-    }
-}
-
-
-waitForThreeSecond()
-
-function register() {
-
-    console.log("user register")
-    waitForThreeSecond()
-}
-function sendemail() {
-    console.log("email send")
-    waitForThreeSecond()
+//     }
+// }
 
 
-}
-function login() {
-    console.log("user can login")
-    waitForThreeSecond()
+// waitForThreeSecond()
 
-}
-function getUserData() {
+function register(cb) {
+
     setTimeout(() => {
-        console.log("get user data")
+
+        console.log("user register")
+        cb()
+    }, 5000)
+
+}
+function sendemail(cb) {
+    setTimeout(() => {
+        console.log("email send")
+        cb()
 
     }, 3000);
 
+
+}
+function login(cb) {
+    setTimeout(() => {
+        console.log("user can login")
+        cb()
+
+    }, 2000);
+
+}
+function getUserData(cb) {
+    setTimeout(() => {
+        console.log("get user data")
+        cb()
+
+    }, 1000);
+
 }
 function displayUserData() {
-
-    console.log("display user data")
+    setTimeout(() => {
+        console.log("display user data")
+    }, 1000);
 }
+register(function () {
+    sendemail(function () {
+        login(function () {
+            getUserData(function () {
+                displayUserData(function () {
 
-register()
-sendemail()
-login()
-getUserData()
-displayUserData()
+                })
+
+            })
+        })
+
+    })
+
+})
+
+
+console.log("other application work")
